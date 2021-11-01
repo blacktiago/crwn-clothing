6ecc2300dd665758361fadeda64069051daaf32a
blacktiago <santiago.plascencia@modinter.com.ec>
2021-11-01 16:35:40 -0500

    Ad raact router dom with HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS</h1>
+  </div>
+);
 
 function App() {
   return (
-    <HomePage></HomePage>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route path='/hats' component={HatsPage}></Route>
+      </Switch>
+    </div>
   );
 }
 
